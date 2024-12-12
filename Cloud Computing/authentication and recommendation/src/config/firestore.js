afd3d285566f6ce5471d1dad993de4809d7114ec
tanayafh-10 <128033228+tanayafh-10@users.noreply.github.com>
2024-12-12 22:29:12 +0800

    Delete Cloud Computing/authentication and recommendation/src directory

--- Cloud Computing/authentication and recommendation/src/config/firestore.js
+++
@@ -1,8 +0,0 @@
-const { Firestore } = require('@google-cloud/firestore');
-
-// Pastikan Anda memiliki credential Google Cloud yang valid
-const firestore = new Firestore({
-    projectId: 'intricate-gamma-443612-g6', // Ganti dengan Project ID Anda
-});
-
-module.exports = firestore;
--- Cloud Computing/authentication and recommendation/src/utils/responseHelper.js
+++
@@ -1,2 +0,0 @@
-exports.success = (h, message, data, code = 200) => h.response({ message, data }).code(code);
-exports.error = (h, message, error, code = 500) => h.response({ message, error }).code(code);
