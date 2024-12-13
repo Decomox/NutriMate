607dfc1a9e33c1f2243947a9fca136a1f653767c
Muhammad Syifa Ridhoni <syifaridhoni02@gmail.com>
2024-12-13 14:13:23 +0700

    reset folder

--- Mobile Development (App)/app/src/main/java/com/dicoding/nutrimate/data/ProgressRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.nutrimate.data
-
-data class ProgressRequest(
-    val height: String,
-    val weight: String,
-    val status: String
-)
--- Mobile Development (App)/app/src/main/java/com/dicoding/nutrimate/data/RegisterRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.nutrimate.data
-
-data class RegisterRequest(
-    val name: String,
-    val email: String,
-    val password: String
-)
